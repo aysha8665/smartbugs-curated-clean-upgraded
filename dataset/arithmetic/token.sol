aab88bf174dec5d22b725cfa7daa2a54fe381756
aysha8665 <aysha.a@zoho.com>
2025-12-24 14:05:33 -0500

    Keep Vulnerable

--- dataset/arithmetic/token.sol
+++ dataset/arithmetic/token.sol
@@ -16,12 +16,13 @@
    }
 
    function transfer(address _to, uint _value) public returns (bool) {
-     
-     require(balances[msg.sender] - _value >= 0);
-     
-     unchecked { balances[msg.sender] -= _value; }
-     unchecked { balances[_to] += _value; }
-     return true;
+    unchecked {
+      require(balances[msg.sender] - _value >= 0);
+
+        balances[msg.sender] -= _value; 
+        balances[_to] += _value; 
+    }
+      return true;
    }
 
    function balanceOf(address _owner) public view returns (uint balance) {
