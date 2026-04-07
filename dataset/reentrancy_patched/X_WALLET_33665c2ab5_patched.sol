ce542e5b381fafc15924f583895cc271827a3d97
aysha8665 <aysha.a@zoho.com>
2026-04-07 16:36:39 -0400

    patched

--- dataset/reentrancy_patched/X_WALLET_33665c2ab5_patched.sol
+++ dataset/reentrancy_patched/X_WALLET_33665c2ab5_patched.sol
@@ -18,19 +18,21 @@ contract X_WALLET
         LogFile.AddMessage(msg.sender,msg.value,"Put");
     }
 
-    function Collect(uint _am)
-    public
-    payable
-    {
+    function Collect(uint _am) public payable {
         Holder storage acc = Acc[msg.sender];
-        if( acc.balance>=MinSum && acc.balance>=_am && block.timestamp>acc.unlockTime)
-        {
+        if( acc.balance>=MinSum && acc.balance>=_am && block.timestamp>acc.unlockTime) {
             
+            // 1. EFFECT (State updated first)
             acc.balance-=_am;
-            (bool success, ) = msg.sender.call{value: _am}(""); if(success)
-            {
-                LogFile.AddMessage(msg.sender,_am,"Collect");
-            }
+            
+            // 2. INTERACTION (External call made safely)
+            (bool success, ) = msg.sender.call{value: _am}(""); 
+            
+            // 3. DEFENSE (Revert state if transfer fails)
+            require(success, "Transfer failed");
+            
+            // 4. LOGGING
+            LogFile.AddMessage(msg.sender,_am,"Collect");
         }
     }
 
