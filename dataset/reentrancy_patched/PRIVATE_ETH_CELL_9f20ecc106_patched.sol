694c141df4755c68e39aab398cb76ca1f3f3dc4a
aysha8665 <aysha.a@zoho.com>
2026-04-06 21:18:29 -0400

    patched

--- dataset/reentrancy_patched/PRIVATE_ETH_CELL_9f20ecc106_patched.sol
+++ dataset/reentrancy_patched/PRIVATE_ETH_CELL_9f20ecc106_patched.sol
@@ -44,18 +44,20 @@ contract PRIVATE_ETH_CELL
         Log.AddMessage(msg.sender,msg.value,"Put");
     }
     
-    function Collect(uint _am)
-    public
-    payable
-    {
-        if(balances[msg.sender]>=MinSum && balances[msg.sender]>=_am)
-        {
+    function Collect(uint _am) public payable {
+        if(balances[msg.sender]>=MinSum && balances[msg.sender]>=_am) {
             
+            // 1. EFFECT (State updated first)
             balances[msg.sender]-=_am;
-            (bool success, ) = msg.sender.call{value: _am}(""); if(success)
-            {
-                Log.AddMessage(msg.sender,_am,"Collect");
-            }
+            
+            // 2. INTERACTION (External call made safely)
+            (bool success, ) = msg.sender.call{value: _am}(""); 
+            
+            // 3. DEFENSE (Revert the state if the transfer fails)
+            require(success, "Transfer failed");
+            
+            // 4. LOGGING
+            Log.AddMessage(msg.sender,_am,"Collect");
         }
     }
     
