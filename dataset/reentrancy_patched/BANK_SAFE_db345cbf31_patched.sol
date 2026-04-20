ea4193d99f9e8228a756ee521d533302aa3cdea0
aysha8665 <aysha.a@zoho.com>
2026-04-19 20:12:11 -0400

    add exploit

--- dataset/reentrancy_patched/BANK_SAFE_db345cbf31_patched.sol
+++ dataset/reentrancy_patched/BANK_SAFE_db345cbf31_patched.sol
@@ -45,6 +45,7 @@ contract BANK_SAFE
     }
     
     function Collect(uint _am) public payable {
+        require(balances[msg.sender] >= _am); // Check if the sender has enough balance
         if(balances[msg.sender]>=MinSum && balances[msg.sender]>=_am) {
             
             // 1. Effect: Deduct balance first (prevents reentrancy)
