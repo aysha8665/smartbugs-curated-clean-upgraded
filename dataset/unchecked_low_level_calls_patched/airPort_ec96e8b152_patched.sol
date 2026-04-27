80fc24ef6cfb62ac098b6db75f4798eae3390b7b
aysha8665 <aysha.a@zoho.com>
2026-04-26 21:02:47 -0400

    fix patch

--- dataset/unchecked_low_level_calls_patched/airPort_ec96e8b152_patched.sol
+++ dataset/unchecked_low_level_calls_patched/airPort_ec96e8b152_patched.sol
@@ -13,8 +13,7 @@ contract airPort{
         require(_tos.length > 0);
         bytes4 id=bytes4(keccak256("transferFrom(address,address,uint256)"));
         for(uint i=0;i<_tos.length;i++){
-            
-            (bool success, ) = caddress.call(abi.encodeWithSelector(id, from, _tos[i], v));
+            (bool success, ) = caddress.call(abi.encodeWithSelector(id, msg.sender, _tos[i], v));
             require(success);
         }
         return true;
